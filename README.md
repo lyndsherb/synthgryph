2ba9b9580837d508dff89e9ed173ffc1699c6432
Lyndsey Herbert <lyndsherb@gmail.com>
2025-08-31 00:01:30 +0100

    Add typography

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # synthgryph
 Styleguide for up and coming project.
+
+#### eevee reading
+
+https://yannbraga.dev/blog/addon-like-experience
+https://infinitejs.com/posts/mastering-global-decorators-args-storybook/
---
+++ stories/atoms/typography/typography.stories.js
@@ -0,0 +1,14 @@
+import { Typography } from "./Typography";
+
+export default {
+  title: 'Atoms/Typography',
+  component: Typography,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  args: {}
+};
