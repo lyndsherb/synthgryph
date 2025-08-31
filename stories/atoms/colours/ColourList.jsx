01cbfba2d43e34aaddde8b902d62a064bb64b9a4
Lyndsey Herbert <lyndsherb@gmail.com>
2025-08-31 01:11:27 +0100

    Add test styles for funsies, improve filter to get computed styles

--- stories/atoms/colours/ColourList.jsx
+++ stories/atoms/colours/ColourList.jsx
@@ -4,25 +4,14 @@ import './colourList.css';
 export const ColourList = () => {
     const colours = useMemo(() => {
         const computed = window.getComputedStyle(document.body);
-        const colours = [];
 
-        // https://stackoverflow.com/a/78242666/7447859
-        for(let i = 0; i < computed.length; i++) {
-            const prop = computed[i];
-
-            if (prop.startsWith('--color') || prop.startsWith('--colour')) {
-                colours.push({
-                    key: prop,
-                    name: prop.replace(/--colo(u?)r-/, ''),
-                    value: computed.getPropertyValue(prop),
-                });
-            }
-        }
-
-        // reverse to get colours in the right order;
-        // since it's cascading stylesheets, we get everything from the lowest
-        // line of code going up to the highest
-        return colours.reverse();
+        return [...computed]
+            .filter((prop) => prop.startsWith('--color') || prop.startsWith('--colour'))
+            .map((prop) => ({
+                key: prop,
+                name: prop.replace(/--colo(u?)r-/, ''),
+                value: computed.getPropertyValue(prop),
+            })).reverse();
     }, []);
     
     return (
