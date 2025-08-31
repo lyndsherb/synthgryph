0ad5e9bf918029cd39fa0f69ccfde79dd5b13c3b
Lyndsey Herbert <lyndsherb@gmail.com>
2025-08-31 03:28:28 +0100

    Add color-convert to showcase all colour options

--- stories/atoms/colours/ColourList.jsx
+++ stories/atoms/colours/ColourList.jsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react'; 
 import './colourList.css';
+import convert from 'color-convert';
 
 export const ColourList = () => {
     const colours = useMemo(() => {
@@ -7,20 +8,55 @@ export const ColourList = () => {
 
         return [...computed]
             .filter((prop) => prop.startsWith('--color') || prop.startsWith('--colour'))
-            .map((prop) => ({
-                key: prop,
-                name: prop.replace(/--colo(u?)r-/, '').replace(/-/g, ' ').replace(/_/g, ' '),
-                value: computed.getPropertyValue(prop),
-            })).reverse();
+            .map((prop) => {
+                const value = computed.getPropertyValue(prop);
+
+                const valueType = (() => {
+                    if (value.startsWith(('#'))) {
+                        return 'hex';
+                    }
+
+                    if (value.startsWith('rgb')) {
+                        return 'rgb';
+                    }
+
+                    return 'keyword';
+                })()
+
+                return {
+                    key: prop,
+                    name: prop.replace(/--colo(u?)r-/, '').replace(/-/g, ' ').replace(/_/g, ' '),
+                    values: [
+                        {
+                            type: 'hex',
+                            value: valueType === 'hex' ? value : `#${convert[valueType].hex(value)}`,
+                        },
+                        {
+                            type: 'rgb',
+                            value: valueType === 'rgb' ? value : convert[valueType].rgb(value).join(', '),
+                        },
+                        {
+                            type: 'keyword',
+                            value: valueType === 'keyword' ? value : convert[valueType].keyword(value),
+                        }
+                    ],
+                };
+            }).reverse();
     }, []);
     
     return (
         <div className="swatchGrid">
             {colours.length ? colours.map(colour => (
                 <div className="swatchItem" key={colour.key}>
-                    <div className="swatch" style={{ backgroundColor: colour.value }} />
+                    <div className="swatch" style={{ backgroundColor: colour.values.find(val => val.type === 'hex').value }} />
                     <h4 className="swatchTitle">{colour.name}</h4>
-                    <code>{colour.value}</code>
+                    <ul>
+                        {colour.values.map(val => (
+                            <li key={`${colour.key}.${val.type}`}>
+                                <b>{val.type}</b>: <code>{val.value}</code>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             )) : <>No colours found</>}
         </div>
